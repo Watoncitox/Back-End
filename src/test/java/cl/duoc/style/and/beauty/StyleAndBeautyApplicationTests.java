f301aca29539f4102e9cc0d76c61b51b85c7b290
Bastian Sanches <ba.sanches@duocuc.cl>
2025-11-17 19:37:44 -0300

    Inicio BackEnd

---
+++ src/test/java/cl/duoc/style/and/beauty/StyleAndBeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.style.and.beauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleAndBeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
